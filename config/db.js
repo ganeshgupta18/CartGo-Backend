f5d6d769a1fa11e6d3f2ee6a0c5e53375add85dd
GANESH GUPTA <ganesh739825@gmail.com>
2026-06-22 21:55:29 +0530

    Add 5-second fast-fail timeout to Mongoose connection

--- config/db.js
+++ config/db.js
@@ -16,6 +16,7 @@ const connectDB = async () => {
     console.log('Connecting to MongoDB...');
     const opts = {
       bufferCommands: false,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of 30
     };
     cachedConnection = await mongoose.connect(process.env.MONGO_URI, opts);
     console.log(`MongoDB Connected: ${cachedConnection.connection.host}`);
